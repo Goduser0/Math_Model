a0aac29d2ff94e6369cbe90bfb4de675ec0f74bb
NS_Alienware <NS_Alienware@zq.com>
2022-09-19 23:20:27 +0800

    1

--- Study_Numpy/class1.py
+++ Study_Numpy/class1.py
@@ -1,16 +1,7 @@
+# NumPy Ndarray
 import pandas as pd
 import numpy as np
 a = np.array([[1, 2], [4, 5], [7, 8]], dtype=complex)
 print(a)
-b = a.reshape((2, 3))
-print(a, '\n', b)
-
-c = np.empty([2, 3], dtype=int, order='C')
-print(c)
-
-d = np.zeros(5)
-print(d)
-
-e = np.zeros([3, 2])
-print(e[0])
-
+b=np.array([1,2,3,4,5], ndmin=2)
+print(b)
