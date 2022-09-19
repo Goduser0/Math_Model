fadf245584950c5dce34b797388adc9df0ca00d8
NS_Alienware <NS_Alienware@zq.com>
2022-09-19 13:29:51 +0800

    add_1

--- README.md
+++ README.md
@@ -1,4 +1,2 @@
 # MathModel
 用于数模比赛的学习与比赛
-
-# This is a test
---
+++ Study_Numpy/class1.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import numpy as np
+a = np.array([[1, 2], [4, 5], [7, 8]], dtype=complex)
+print(a)
+b = a.reshape((2, 3))
+print(a, '\n', b)
+
+c = np.empty([2, 3], dtype=int, order='C')
+print(c)
+
+d = np.zeros(5)
+print(d)
+
+e = np.zeros([2, 3])
+print(e[0])
+
