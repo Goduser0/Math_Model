c49997cd7227f752b2458666b2aa1a0b8176df99
NS_Alienware <NS_Alienware@zq.com>
2022-09-23 18:27:19 +0800

    1

---
+++ figmaking/color_set1.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+import matplotlib.colors as mpcolors
+
+# 颜色表示
+# RGB/RGBA 元组格式: (0.1,0.2,0.5)/(0.1,0.2,0.5,0.3)
+# RGB/RGBA hex格式: '#0F0F0F'/'#0F0F0F0F'
+# 0~1 float: '0.6'
+# 基本色: 'b' 'g' 'r' 'c' 'm' 'y' 'k' 'w'
+
+# 内置颜色
+# matplotlib.colors.BASE_COLORS
+# matplotlib.colors.TABLEAU_COLORS
+# matplotlib.colors.CSS4_COLORS
+# matplotlib.colors.XKCD_COLORS
+print(mpcolors.CSS4_COLORS)
+
